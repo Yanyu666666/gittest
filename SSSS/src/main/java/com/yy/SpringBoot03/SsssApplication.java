4618c4c0b5d8e1c45c4da50ebf653957f6c07a96
yanyu666666 <845572849@qq.com>
2019-06-17 14:47:34 +0800

    创建测试

---
+++ SSSS/src/main/java/com/yy/SpringBoot03/SsssApplication.java
@@ -0,0 +1,13 @@
+package com.yy.SpringBoot03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsssApplication.class, args);
+	}
+
+}
